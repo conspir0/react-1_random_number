ebba5b9f26f7b5a3e291e6497ba5b9beaa602617
conspir0 <michalczyk.mat3usz@gmail.com>
2020-08-27 20:22:29 +0200

    Create project structure

--- App.js
+++ App.js
@@ -1,36 +1,10 @@
 class App extends React.Component {
-  state = {
-    number: ''
-  }
-
-  randomNumber = () => {
-    const minVal = 0,
-      maxVal = 9,
-      sign = Math.floor(Math.random() * (maxVal - minVal + 1) + minVal);
-
-    return sign;
-  }
-
-  buttonHanlder = () => {
-    this.setState({
-      number: this.randomNumber()
-    })
-  }
+  state = {}
 
   render() {
     return (
       <React.Fragment>
-        <div className="container">
-          <button
-            className="button"
-            onClick={this.buttonHanlder}>
-            Click
-          </button>
-          <div
-            className="result-container">
-            {this.state.number}
-          </div>
-        </div>
+        <div>Hello World</div>
       </React.Fragment>
     )
   }
